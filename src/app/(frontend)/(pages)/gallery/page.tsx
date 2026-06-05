33f2cccf99c26e4d8f6413667e9a13d749ebf311
mlchandigarh <tfugchd@gmail.com>
2026-06-05 10:46:58 +0530

    Updated the gallery meta and description

--- src/app/(frontend)/(pages)/gallery/page.tsx
+++ src/app/(frontend)/(pages)/gallery/page.tsx
@@ -4,13 +4,13 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Gallery | D4 Community",
-  description: "Browse photos from our tech events and hackathons.",
+  description: "Browse photos from our events, meetups, and hackathons.",
   alternates: {
     canonical: "https://www.d4community.com/gallery",
   },
   openGraph: {
     title: "D4 Community Gallery",
-    description: "Browse photos from our tech events and hackathons.",
+    description: "Browse photos from our events, meetups, and hackathons.",
     url: "https://www.d4community.com/gallery",
     siteName: "D4 Community",
     type: "website",
